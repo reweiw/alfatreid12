cbf328e8b4ca0fb90299a26585ba7bfaadf8a04b
reweiw <a3bm@ya.ru>
2024-08-21 19:57:46 +0300

    Add files via upload

---
+++ css3-mediaqueries.js
@@ -0,0 +1,4 @@
+// Подключаем CSS3 Media Queries Javascript для IE8 и старше
+<!--[if lt IE 9]>
+	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+<![endif]-->
